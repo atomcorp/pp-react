07ec73dbef3f548bc195c8c5b5fe6ebcfd13075c
atomcorp <thomasmaxwellsmith@gmail.com>
2017-07-28 23:59:10 +0100

    made a static react app that lists football fixtures

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './App.css';
+// fixtures is an array of fixture objects
+import fixtures from './data/fixtures.js';
+
+class App extends Component {
+
+  render() {
+    return (
+      // we will return 
+      // - <FixtureList />
+      // --- <Fixture />
+      // ----- Home & Away Team 
+      <div className="fixture-list">
+        <FixtureList fixtures={fixtures} />
+      </div>
+    );
+  }
+}
+
+function FixtureList(props) {
+
+  var myArray = [];
+  props.fixtures.forEach(function(fixture) {
+    myArray.push(<Fixture key={fixture.id} home={fixture.home} away={fixture.away} />);
+  });
+
+
+  // this will need to print one fixture for length of fixture list
+  return (
+    <table className="table table-striped table-bordered">
+      <thead>
+        <tr>
+          <th>Home</th>
+          <th></th>
+          <th></th>
+          <th>Away</th>
+        </tr>
+      </thead>
+      <tbody> 
+        {myArray}
+      </tbody>
+    </table>
+
+
+  );
+}
+
+function Fixture(props) {
+  // props will be fixture
+  // take 2 team names
+  // and print the tds 
+  return (
+    <tr>
+      <td>{props.home}</td>
+      <td>0</td>
+      <td>0</td>
+      <td>{props.away}</td>
+    </tr>
+  );
+}
+
+
+export default App;
---
+++ src/data/fixtures.js
@@ -0,0 +1,14 @@
+const fixtures = [
+  {"home":"Manchester United ","away":"Swansea City","date":"2014-08-16","time":"12:45","id":1,"gameweek":1},
+  {"home":"Leicester City ","away":"Everton","date":"2014-08-16","time":"15:00","id":2,"gameweek":1},
+  {"home":"Queens Park Rangers ","away":"Hull City","date":"2014-08-16","time":"15:00","id":3,"gameweek":1},
+  {"home":"Stoke City ","away":"Aston Villa","date":"2014-08-16","time":"15:00","id":4,"gameweek":1},
+  {"home":"West Bromwich Albion ","away":"Sunderland","date":"2014-08-16","time":"15:00","id":5,"gameweek":1},
+  {"home":"West Ham United ","away":"Tottenham Hotspur","date":"2014-08-16","time":"15:00","id":6,"gameweek":1},
+  {"home":"Arsenal ","away":"Crystal Palace","date":"2014-08-16","time":"17:30","id":7,"gameweek":1},
+  {"home":"Liverpool ","away":"Southampton","date":"2014-08-17","time":"13:30","id":8,"gameweek":1},
+  {"home":"Newcastle United ","away":"Manchester City","date":"2014-08-17","time":"16:00","id":9,"gameweek":1},
+  {"home":"Burnley ","away":"Chelsea","date":"2014-08-18","time":"20:00","id":10,"gameweek":1}
+];
+
+export default fixtures;
