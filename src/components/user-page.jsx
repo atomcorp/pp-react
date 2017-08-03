a832de5e4e2735b80e13a2c5d8fada6c40d01b10
atomcorp <thomasmaxwellsmith@gmail.com>
2017-08-03 19:29:26 +0100

    added simple user page

--- src/components/user-page.jsx
+++ src/components/user-page.jsx
@@ -3,6 +3,11 @@ import React, {Component} from 'react';
 export default function UserPage(props) {
   console.log(props)
   return(
-    <div>{ props.user.name }</div>
+    <div class="user">
+      <h1>User page</h1>
+      <div className="user__name">Name: { props.user.name }</div>
+      <div className="user__team">Team : { props.user.team }</div>
+      <div className="user__points">Points : { props.user.points }</div>
+    </div>
   );
 }
