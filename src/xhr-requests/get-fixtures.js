3d7019a79c2c6951ddd7ce693f5ee4a4b4d0e775
atomcorp <thomasmaxwellsmith@gmail.com>
2017-08-05 16:24:32 +0100

    re-added predictions from firebase

--- src/xhr-requests/get-fixtures.js
+++ src/xhr-requests/get-fixtures.js
@@ -6,59 +6,51 @@ export default function getFixturesFromFirebase(uid, gameData, callback) {
   const header = { 
     headers: { 'X-Auth-Token': footballDataOrg }
   };
+
+
+  // https://stackoverflow.com/questions/33178738/how-to-execute-multiple-firebase-request-and-receive-a-callback-when-all-request
+  const predictionsRefString = `/usersPredictions/${uid}/${gameData.season}/${gameData.gameweek}/predictions`;
+  const predictions = db.ref(predictionsRefString);
+
   const cancelablePromise = makeCancelable(
-  fetch('http://api.football-data.org/v1/competitions/445/fixtures/?matchday=1', header)
-    .then(function(response) {
-      // send request to the api for browser to check we're allowed
-      if (response.status === 200) {
-        return response.json();
-      }
-      else throw new Error('Something went wrong on api server!');
-    })
-    .then(function(response) {
-      // if succesful we get data!
-      console.log(response);
-      const fixtures = response.fixtures.reduce(function(obj, item, index) {
-        // obj is the {} at the end of reduce
-        // each loop adds a new obj
-        obj = Object.assign(obj, {[`fixture${index}`]: item})
-        return obj;
-      }, {});
+    // this all mighty mess makes 
+    // 1. call to football-data for fixtures
+    // 2. call to firebase for predictions (if available)
+    Promise.all([
+      fetch('http://api.football-data.org/v1/competitions/445/fixtures/?matchday=1', header)
+        .then(function(response) {
+          // send request to the api for browser to check we're allowed
+          if (response.status === 200) {
+            return response.json();
+          }
+          else throw new Error('Something went wrong on api server!');
+        })
+        .then(function(response) {
+          // if succesful we get data!
+          // convert the array we receive into an object
+          // and assign an id
+          const fixtures = response.fixtures.reduce(function(obj, item, index) {
+            // obj is the {} at the end of reduce
+            // each loop adds a new obj
+            obj = Object.assign(obj, {[`fixture${index}`]: item})
+            return obj;
+          }, {});
+          return fixtures;
+        }),
+      new Promise((resolve, reject) => {
+        predictions.on('value', (snapshot) => {
+          resolve(snapshot.val());
+        })
+      }),
+    ]).then((data) => {
+    // arrays return in order
       const dataObject = {
-        fixtures: fixtures,
-        predictions: null
+        fixtures: data[0],
+        predictions: data[1]
       }
       callback(dataObject);
     })
   );
-
-  // https://stackoverflow.com/questions/33178738/how-to-execute-multiple-firebase-request-and-receive-a-callback-when-all-request
-  const fixturesRefString = `/${gameData.season}/${gameData.gameweek}/`;
-  const predictionsRefString = `/usersPredictions/${uid}/${gameData.season}/${gameData.gameweek}/predictions`;
-  const fixtures = db.ref(fixturesRefString);
-  const predictions = db.ref(predictionsRefString);
-
-  // const cancelablePromise = makeCancelable(
-  //   Promise.all([
-  //       new Promise((resolve, reject) => {
-  //         fixtures.on('value', (snapshot) => {
-  //           resolve(snapshot.val());
-  //         })
-  //       }),
-  //       new Promise((resolve, reject) => {
-  //         predictions.on('value', (snapshot) => {
-  //           resolve(snapshot.val());
-  //         })
-  //       }),
-  //     ]).then((data) => {
-  //     // arrays return in order
-  //     const dataObject = {
-  //       fixtures: data[0],
-  //       predictions: data[1]
-  //     }
-  //     callback(dataObject);
-  //   })
-  // );
 
   cancelablePromise
     .promise
