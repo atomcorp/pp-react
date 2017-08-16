73ce0c76bc845dc8bb6e15df561088255d8383f5
atomcorp <thomasmaxwellsmith@gmail.com>
2017-08-16 23:07:26 +0100

    beginning of moving away from calling footbal-data api (too complex managing 2 calls), using static data from api

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import {auth, storageKey} from './firebase-connect.js';
 
 // import SetFixtures from './xhr-requests/set-fixtures';
 // SetFixtures();
+
 class App extends Component {
   constructor(props) {
     super(props);
